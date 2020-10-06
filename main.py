1caf589bd67b8a046a6cc6489b0f1345c8ae8730
Restyled.io <commits@restyled.io>
2020-10-06 13:42:57 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -1,64 +1,93 @@
 # Imports
 import argparse
 from datetime import datetime
+
 # Import local scripts
 import read_write_serial
 import print_logging
+
 # Define
 print_log = print_logging.print_logging
 
 
-class color():
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    N = '\033[0m'
+class color:
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    UNDERLINE = "\033[4m"
+    N = "\033[0m"
 
 
 # Arguments
 argparser = argparse.ArgumentParser(
-    description='SMLParser is an Python based SML(OBIS) Parser')
-requiredNamed = argparser.add_argument_group('required arguments')
-requiredNamed.add_argument('-sp', '--serialport', help='Serial Port')
-argparser.add_argument('-lf', '--log-file', help='Defines the log file')
+    description="SMLParser is an Python based SML(OBIS) Parser"
+)
+requiredNamed = argparser.add_argument_group("required arguments")
+requiredNamed.add_argument("-sp", "--serialport", help="Serial Port")
+argparser.add_argument("-lf", "--log-file", help="Defines the log file")
 argparser.add_argument(
-    '-v', '--log-level', help='Loglevel 0=Off 1= Error 2=Info 3=Debug 4=Everything - Default 2')
+    "-v",
+    "--log-level",
+    help="Loglevel 0=Off 1= Error 2=Info 3=Debug 4=Everything - Default 2",
+)
 
 args = argparser.parse_args()
 # Loglevel 0=Off 1= Error 2=Info 3=Debug 4=Everything - Default 2
 if args.log_level:
     if int(args.log_level) >= 1:
-        print("{}[i]{} Loglevel is: {}{}{}".format(color.GREEN,
-                                                   color.N, color.YELLOW, args.log_level, color.N))
+        print(
+            "{}[i]{} Loglevel is: {}{}{}".format(
+                color.GREEN, color.N, color.YELLOW, args.log_level, color.N
+            )
+        )
     loglevel = args.log_level
 else:
     loglevel = 2
 
 if not args.serialport:
     if int(loglevel) >= 1:
-        print_log("{}[!]{} You need to define the Serialport! {}-sp [SerialPort]{}".format(
-            color.RED, color.N, color.YELLOW, color.N), loglevel, args)
+        print_log(
+            "{}[!]{} You need to define the Serialport! {}-sp [SerialPort]{}".format(
+                color.RED, color.N, color.YELLOW, color.N
+            ),
+            loglevel,
+            args,
+        )
     quit()
 # Print Serial-Port
-print_log("{}[i]{} Serial Port is: {}{}{}".format(
-    color.GREEN, color.N, color.YELLOW, args.serialport, color.N), loglevel, args)
+print_log(
+    "{}[i]{} Serial Port is: {}{}{}".format(
+        color.GREEN, color.N, color.YELLOW, args.serialport, color.N
+    ),
+    loglevel,
+    args,
+)
 
 
 if args.log_file:
-    print_log("{}[i]{} Log file-path is: {}{}{}".format(color.GREEN,
-                                                        color.N, color.YELLOW, args.log_file, color.N), loglevel, args)
+    print_log(
+        "{}[i]{} Log file-path is: {}{}{}".format(
+            color.GREEN, color.N, color.YELLOW, args.log_file, color.N
+        ),
+        loglevel,
+        args,
+    )
 
 # Print Date and time
 now = datetime.now()
 now_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-print_log("{}[i]{} Started at: {}{}{}".format(color.GREEN, color.N,
-                                              color.YELLOW, now_date_time, color.N), loglevel, args)
+print_log(
+    "{}[i]{} Started at: {}{}{}".format(
+        color.GREEN, color.N, color.YELLOW, now_date_time, color.N
+    ),
+    loglevel,
+    args,
+)
 
 # Testing
-#print(read_write_serial.readSerial(args.serialport, 100))
+# print(read_write_serial.readSerial(args.serialport, 100))
--- print_logging.py
+++ print_logging.py
@@ -5,8 +5,8 @@ def log(text, loglevel, args):
     with open(args.log_file, "a") as log_file:
         log_file.write("\n")
         # Remove Ascii Escape(Colors)
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-        result = ansi_escape.sub('', text)
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+        result = ansi_escape.sub("", text)
         log_file.write(result)
 
 
